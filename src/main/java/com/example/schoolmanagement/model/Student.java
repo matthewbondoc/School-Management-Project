b5d8f90245b6c9ad6e80a44aba3a2983edaaff1e
mahttewbondoc <mahttewbondoc5@gmail.com>
2021-11-15 18:32:25 +0800

    student java

---
+++ src/main/java/com/example/schoolmanagement/model/Student.java
@@ -0,0 +1,40 @@
+package com.example.schoolmanagement.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "students")
+public class Student {
+
+    @Id
+    private int idNumber;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
