a0401d4ba0a9ee14442bdad7af7098d8963192dd
mahttewbondoc <mahttewbondoc5@gmail.com>
2021-11-16 21:18:34 +0800

    fixing cors

--- src/main/java/com/example/schoolmanagement/controller/StudentController.java
+++ src/main/java/com/example/schoolmanagement/controller/StudentController.java
@@ -6,6 +6,7 @@ import com.example.schoolmanagement.data.StudentData;
 import com.example.schoolmanagement.model.Student;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class StudentController {
 
         @Autowired
